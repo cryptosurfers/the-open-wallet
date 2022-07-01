bfa61d82a3d14621fd75da3e6678409ad08fb420
pinosol <zonest_64@mail.ru>
2022-07-02 02:46:58 +0300

    init gh

---
+++ gh-pages.js
@@ -0,0 +1,17 @@
+const ghpages = require('gh-pages');
+
+ghpages.publish(
+ 'build', // path to public directory
+ {
+  branch: 'gh-pages',
+  repo: 'https://github.com/cryptosurfers/the-open-wallet.git', // Update to point to your repository
+  user: {
+   name: 'faintsmi1e', // update to use your name
+   email: 'zonest_64@mail.ru' // Update to use your email
+  },
+  dotfiles: true
+  },
+  () => {
+   console.log('Deploy Complete!');
+  }
+);
