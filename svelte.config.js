82b87e36d05f54a1902a047d9492227ec18055f9
pinosol <zonest_64@mail.ru>
2022-07-17 19:59:00 +0300

    add tonweb, tonweb-mnemonic types

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,26 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static'
+import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess({ postcss: true }),
 
-	kit: {
-		adapter: adapter(),
+  kit: {
+    adapter: adapter({
+      fallback: 'index.html'
+    }),
 
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
-		}
-	}
-};
+    prerender: {
+      enabled: false
+    },
 
-export default config;
+    // Override http methods in the Todo forms
+    methodOverride: {
+      allowed: ['PATCH', 'DELETE']
+    }
+  }
+}
+
+export default config
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
-import { sveltekit } from '@sveltejs/kit/vite';
+import { sveltekit } from '@sveltejs/kit/vite'
+import { resolve } from "path";
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
-};
+  plugins: [sveltekit()],
+  resolve: {
+    alias: {
+      'tonweb': resolve('./node_modules/tonweb/dist/tonweb.js')
+    }
+  }
+}
 
-export default config;
+export default config
