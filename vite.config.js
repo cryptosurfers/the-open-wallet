63849f9b006e8009ae01f30780419333dfd3939f
pinosol <zonest_64@mail.ru>
2022-07-18 15:33:46 +0300

    add action

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite'
-import { resolve } from "path";
+import { resolve } from 'path'
 
 /** @type {import('vite').UserConfig} */
 const config = {
   plugins: [sveltekit()],
   resolve: {
     alias: {
-      'tonweb': resolve('./node_modules/tonweb/dist/tonweb.js')
+      tonweb: resolve('./node_modules/tonweb/dist/tonweb.js')
     }
   }
 }
